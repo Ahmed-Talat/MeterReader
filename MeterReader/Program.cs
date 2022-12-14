a64ee2a10a8de4fdd192cb749338ef76dc2658a7
Ahmed Talat <talat.salem@gmail.com>
2022-12-14 12:20:25 +0200

    Add gRPC

--- MeterReader/Program.cs
+++ MeterReader/Program.cs
@@ -1,5 +1,6 @@
 using MeterReader;
 using MeterReader.Repos;
+using MeterReader.Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +10,14 @@ builder.Services.AddDbContext<ReadingContext>(opt =>
     opt.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]);
 });
 
+builder.Services.AddGrpc(opt => opt.EnableDetailedErrors = true);
+builder.Services.AddGrpcReflection();
+
 builder.Services.AddScoped<IReadingRepository, ReadingRepository>();
 
 var app = builder.Build();
+
+app.MapGrpcService<MeterReadingService>();
+app.MapGrpcReflectionService();
 
 app.Run();
---
+++ MeterReader/Service/MeterReadingService.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using MeterReader.Entities;
+using MeterReader.gRPC;
+using MeterReader.Repos;
+using static MeterReader.gRPC.MeterReadingService;
+
+namespace MeterReader.Service;
+
+public class MeterReadingService : MeterReadingServiceBase
+{
+    private readonly IReadingRepository _repository;
+    private readonly ILogger<MeterReadingService> _logger;
+
+    public MeterReadingService(IReadingRepository repository, ILogger<MeterReadingService> logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+
+    public override async Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+    {
+        if (request.Successful == ReadingStatus.Success)
+        {
+            foreach (var reading in request.Readings)
+            {
+                var readingValue = new MeterReading()
+                {
+                    CustomerId = reading.CustomerId,
+                    Value = reading.ReadingValue,
+                    ReadingDate = reading.ReadingTime.ToDateTime()
+                };
+
+                _repository.AddEntity(readingValue);
+            }
+
+            if (await _repository.SaveAllAsync())
+            {
+                return new StatusMessage()
+                {
+                    Message = "Successfully added to the database.",
+                    Success = ReadingStatus.Success
+                };
+            }
+        }
+
+        return new StatusMessage()
+        {
+            Message = "Failed to store readings in Database",
+            Success = ReadingStatus.Success
+        };
+    }
+}
